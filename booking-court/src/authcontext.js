ad0632604a6eec1b2930f5c22b1ed61d2d3d2dbb
Sothea-SQA <ban.sothea@squeeze-inc.asia>
2025-03-03 15:17:10 +0700

    first Commit

---
+++ booking-court/src/authcontext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
